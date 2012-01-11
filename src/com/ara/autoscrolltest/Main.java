9e49fd3b2a70373462afd070ac8872b23f6436d2
Rick Barkhouse <rick.barkhouse@gmail.com>
2012-01-10 22:58:44 -0500

    first commit

---
+++ src/com/ara/autoscrolltest/Main.java
@@ -0,0 +1,48 @@
+package com.ara.autoscrolltest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+	public class Main extends Activity {
+	    /** Called when the activity is first created. */
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.main);
+	        
+			Button b = (Button) findViewById(R.id.addButton);
+			b.setOnClickListener(new AddListener());
+			
+			add();
+	    }
+	    
+	    private void add() {
+			LinearLayout l = (LinearLayout) findViewById(R.id.list);
+			final HorizontalScrollView s = (HorizontalScrollView) findViewById(R.id.scroller);
+			
+			ImageView i = new ImageView(getApplicationContext());
+			i.setImageResource(android.R.drawable.btn_star_big_on);
+			l.addView(i);
+			
+			//s.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+			
+			new Handler().postDelayed(new Runnable() {
+			    public void run() {
+			        s.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+			    }
+			}, 100L);
+	    }
+	    
+		private class AddListener implements View.OnClickListener {
+			@Override
+			public void onClick(View v) {
+				add();
+			}
+		}
+	}
